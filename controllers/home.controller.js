4a3f90c153f42ac47a5ff256c8fcf441f564456b
chinguyen98 <dacchi6761@gmail.com>
2020-02-17 10:00:07 +0700

    Homepage

--- controllers/home.controller.js
+++ controllers/home.controller.js
@@ -1,3 +1,9 @@
+const Category = require('../models/category.model');
+const Flower = require('../models/flower.model');
+
 module.exports.renderHomePage = function (req, res, next) {
-    res.render('home/index', { 'title': 'Blossom Shop' });
+    Flower.find({}, (err, flowers) => {
+        if (err) throw err;
+        res.render('home/index', { 'title': 'Blossom Shop', 'flowers': flowers });
+    })
 }
